5ea067d8cf04671115e19c619d35532e349c7259
Feshchuk Andrew <feshykandrew.work@gmail.com>
2024-01-19 15:01:12 +0100

    Add Bank model and migration

---
+++ database/migrations/2024_01_19_104313_create_banks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('banks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->json('logo')->nullable();
+            $table->string('site')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->string('legal_address')->nullable();
+            $table->float('rating')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('banks');
+    }
+};
